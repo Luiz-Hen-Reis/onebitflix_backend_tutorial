74dc11c3665e5ce3b94bf30962e0c7bc98f95029
Luiz-Hen-Reis <lhcontato2020@gmail.com>
2023-07-09 12:28:09 -0300

    video streaming route

---
+++ src/controllers/episodesController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import path from "path";
+import fs from "fs";
+import { episodeService } from "../services/episodeService";
+
+export const episodesController = {
+  stream: async (req: Request, res: Response) => {
+    const { videoUrl } = req.query;
+
+    try {
+      if (typeof videoUrl !== "string")
+        throw new Error("videoUrl param must be of type string");
+
+      const range = req.headers.range;
+
+      episodeService.streamEpisodeToResponse(res, videoUrl, range);
+    } catch (error) {
+      if (error instanceof Error)
+        return res.status(400).json({ message: error.message });
+    }
+  },
+};
